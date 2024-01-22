b3afddfaf1b19160d653e00801ac63fc129abf08
Ludvig Gunne Lindström <ludvig.gunne.lindstrom@proton.me>
2024-01-22 11:52:01 +0100

    use 256-bit registers

--- main.c
+++ main.c
@@ -9,8 +9,8 @@
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "extern/stb_image_write.h"
 
-#define IMG_WIDTH   1024
-#define IMG_HEIGHT  1024
+#define IMG_WIDTH   2048
+#define IMG_HEIGHT  2048
 #define CENTER_X   -0.75f
 #define CENTER_Y    0.0f
 #define VIEW        3.0f
@@ -52,61 +52,64 @@ void mandelbrot_simd(unsigned char *img)
 {
     for (size_t y = 0; y < IMG_HEIGHT; y++)
     {
-        for (size_t x = 0; x < IMG_WIDTH - 3; x += 4)
+        for (size_t x = 0; x < IMG_WIDTH - 7; x += 8)
         {
 
             /* Calculate pixels corresponding complex values */
-            __m128 cx = _mm_set_ps1((float)x);
-            cx = _mm_add_ps(cx, _mm_set_ps(0.0f, 1.0f, 2.0f, 3.0f));
-            cx = _mm_div_ps(cx, _mm_set_ps1((float)IMG_WIDTH));
-            cx = _mm_sub_ps(cx, _mm_set_ps1(.5f));
-            cx = _mm_mul_ps(cx, _mm_set_ps1(VIEW));
-            cx = _mm_add_ps(cx, _mm_set_ps1(CENTER_X));
+            __m256 cx = _mm256_set1_ps((float)x);
+            cx = _mm256_add_ps(cx, _mm256_set_ps(0.0f, 1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f, 7.0f));
+            cx = _mm256_div_ps(cx, _mm256_set1_ps((float)IMG_WIDTH));
+            cx = _mm256_sub_ps(cx, _mm256_set1_ps(.5f));
+            cx = _mm256_mul_ps(cx, _mm256_set1_ps(VIEW));
+            cx = _mm256_add_ps(cx, _mm256_set1_ps(CENTER_X));
 
-            __m128 cy = _mm_set_ps1((float)y);
-            cy = _mm_div_ps(cy, _mm_set_ps1((float)IMG_HEIGHT));
-            cy = _mm_sub_ps(cy, _mm_set_ps1(.5f));
-            cy = _mm_mul_ps(cy, _mm_set_ps1(VIEW));
-            cy = _mm_add_ps(cy, _mm_set_ps1(CENTER_Y));
+            __m256 cy = _mm256_set1_ps((float)y);
+            cy = _mm256_div_ps(cy, _mm256_set1_ps((float)IMG_HEIGHT));
+            cy = _mm256_sub_ps(cy, _mm256_set1_ps(.5f));
+            cy = _mm256_mul_ps(cy, _mm256_set1_ps(VIEW));
+            cy = _mm256_add_ps(cy, _mm256_set1_ps(CENTER_Y));
 
-            __m128 zx  = _mm_set_ps1(0.0f);
-            __m128 zy  = _mm_set_ps1(0.0f);
+            __m256 zx  = _mm256_set1_ps(0.0f);
+            __m256 zy  = _mm256_set1_ps(0.0f);
 
-            __m128i acc = _mm_set1_epi32(0);
-            __m128  bnd = (__m128)_mm_set1_epi32(0);
+            __m256i acc = _mm256_set1_epi32(0);
+            __m256  bnd = (__m256)_mm256_set1_epi32(0);
 
             size_t i = 256;
 
             while (--i > 0)
             {
                 /* Squared parts */
-                __m128 zx2 = _mm_mul_ps(zx, zx);
-                __m128 zy2 = _mm_mul_ps(zy, zy);
+                __m256 zx2 = _mm256_mul_ps(zx, zx);
+                __m256 zy2 = _mm256_mul_ps(zy, zy);
 
                 /* Determine which values are inside bounds */
-                bnd = _mm_cmp_ps(_mm_add_ps(zx2, zy2), _mm_set_ps1(BOUND), _CMP_LT_OQ);
+                bnd = _mm256_cmp_ps(_mm256_add_ps(zx2, zy2), _mm256_set1_ps(BOUND), _CMP_LT_OQ);
 
                 /* Accumulate pixels inside bounds */
-                acc = _mm_add_epi32(acc, (__m128i)_mm_and_ps(bnd, (__m128)_mm_set1_epi32(1)));
+                acc = _mm256_add_epi32(acc, (__m256i)_mm256_and_ps(bnd, (__m256)_mm256_set1_epi32(1)));
 
                 /* Break if none are inside bounds */
-                int all_outside = _mm_test_all_zeros(_mm_set1_epi32(0xffffffff), (__m128i)bnd);
+                int all_outside = _mm256_testz_si256((__m256i)bnd, _mm256_set1_epi32(-1u));
                 if (all_outside)
                 {
                     break;
                 }
 
                 /* Iterate */
-                __m128 tmp = zx;
-                zx = _mm_add_ps(_mm_sub_ps(zx2, zy2), cx);
-                zy = _mm_add_ps(_mm_mul_ps(_mm_set_ps1(2.0f), _mm_mul_ps(tmp, zy)), cy);
+                __m256 tmp = zx;
+                zx = _mm256_add_ps(_mm256_sub_ps(zx2, zy2), cx);
+                zy = _mm256_add_ps(_mm256_mul_ps(_mm256_set1_ps(2.0f), _mm256_mul_ps(tmp, zy)), cy);
             }
 
-            /* I can't use _mm_cvtepi32_epi8 :(  */
-            img[x + 0 + (IMG_WIDTH * y)] = ((int*)&acc)[3];
-            img[x + 1 + (IMG_WIDTH * y)] = ((int*)&acc)[2];
-            img[x + 2 + (IMG_WIDTH * y)] = ((int*)&acc)[1];
-            img[x + 3 + (IMG_WIDTH * y)] = ((int*)&acc)[0];
+            img[x + 0 + (IMG_WIDTH * y)] = ((int*)&acc)[7];
+            img[x + 1 + (IMG_WIDTH * y)] = ((int*)&acc)[6];
+            img[x + 2 + (IMG_WIDTH * y)] = ((int*)&acc)[5];
+            img[x + 3 + (IMG_WIDTH * y)] = ((int*)&acc)[4];
+            img[x + 4 + (IMG_WIDTH * y)] = ((int*)&acc)[3];
+            img[x + 5 + (IMG_WIDTH * y)] = ((int*)&acc)[2];
+            img[x + 6 + (IMG_WIDTH * y)] = ((int*)&acc)[1];
+            img[x + 7 + (IMG_WIDTH * y)] = ((int*)&acc)[0];
         }
     }
 }
